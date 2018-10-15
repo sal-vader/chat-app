69f893549c2c05ceab8aaeab81b1ec83032fd898
sal <sal.fiesta.7@gmail.com>
2018-10-15 01:43:31 -1000

    added connect and disconnect io events

--- server/app.js
+++ server/app.js
@@ -7,8 +7,18 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 8080;
 let app = express();
 let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
 
 server.listen(port, () => {
     console.log(`Listening on port: ${port}`);
