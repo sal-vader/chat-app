376252e3e1f4d4dc4e78ba82dd81afbef4f750dd
sal <sal.fiesta.7@gmail.com>
2018-10-21 02:47:36 -1000

    added generateLocationMessage test

--- server/tests/utils/message.test.js
+++ server/tests/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('../../utils/message');
+const {generateMessage, generateLocationMessage} = require('../../utils/message');
 
 describe('generateMessage', () => {
     it('should generate correct message object using arguments', () => {
@@ -14,3 +14,16 @@ describe('generateMessage', () => {
         expect(typeof generatedMessage.createdDate).toBe('number');
     });
 }); 
+
+describe('generateLocationMessage', () => {
+    it('should generate a location object', () => {
+        let from = 'testUser';
+        let latitude = 32.12414532;
+        let longitude = 145.1245352;
+        let url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+        let generatedLocationMessage = generateLocationMessage(from, latitude, longitude);
+
+        expect(generatedLocationMessage).toMatchObject({from, url});
+        expect(typeof generatedLocationMessage.createdDate).toBe('number');
+    });
+});
